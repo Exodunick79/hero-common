429b408ff6a316428762948d63a661da279f2ed8
deed821 <deed821@gmail.com>
2018-03-01 05:15:49 +1100

    hero: update before revert

--- hero-common.mk
+++ hero-common.mk
@@ -143,7 +143,12 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
 	libsensoreventq \
+	android.hardware.gnss@1.0-service \
 	android.hardware.gnss@1.0-impl
+
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+	$(DEVICE_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
 # Graphics
 PRODUCT_PACKAGES += \
